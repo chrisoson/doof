34f672cd1096f91d2a58ebbc8f9a9c2998dd1f67
Chris <chris.oson.dev@gmail.com>
2014-01-01 00:00:00 +0500

    Added facebook and microsoft sign in

--- src/Program.cs
+++ src/Program.cs
@@ -4,8 +4,11 @@ using doof.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-var connectionString = builder.Configuration["SQL_SERVER_CONNECTION"];
+//If it is in production, i will read env variables in the docker container
+//provided by a .env file and passed in the docker-compose.yml file.
+var connectionString = builder.Environment.IsProduction()
+    ? Environment.GetEnvironmentVariable("SQL_SERVER_CONNECTION")!
+    : builder.Configuration["SQL_SERVER_CONNECTION"];
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
@@ -16,11 +19,49 @@ builder.Services.AddRazorPages();
 
 var configuration = builder.Configuration;
 
-builder.Services.AddAuthentication().AddGoogle(googleOptions =>
-{
-    googleOptions.ClientId = configuration["GOOGLE_CLIENT_ID"]!;
-    googleOptions.ClientSecret = configuration["GOOGLE_CLIENT_SECRET"]!;
-});
+
+//todo set up all the url of these providers when the site is live.
+builder.Services.AddAuthentication()
+    .AddGoogle(googleOptions =>
+    {
+        if (builder.Environment.IsProduction())
+        {
+            googleOptions.ClientId = Environment.GetEnvironmentVariable("GOOGLE_CLIENT_ID")!;
+            googleOptions.ClientSecret = Environment.GetEnvironmentVariable("GOOGLE_CLIENT_SECRET")!;
+        }
+        else
+        {
+            googleOptions.ClientId = configuration["GOOGLE_CLIENT_ID"]!;
+            googleOptions.ClientSecret = configuration["GOOGLE_CLIENT_SECRET"]!;
+        }
+    })
+    //This is not working right now, set this up when the website is live.
+    .AddFacebook(facebookOptions =>
+    {
+        if (builder.Environment.IsProduction())
+        {
+            facebookOptions.AppId = Environment.GetEnvironmentVariable("FACEBOOK_APP_ID")!;
+            facebookOptions.AppSecret = Environment.GetEnvironmentVariable("FACEBOOK_APP_SECRET")!;
+        }
+        else
+        {
+            facebookOptions.AppId = configuration["FACEBOOK_APP_ID"]!;
+            facebookOptions.AppSecret = configuration["FACEBOOK_APP_SECRET"]!;
+        }
+    })
+    .AddMicrosoftAccount(microsoftOptions =>
+    {
+        if (builder.Environment.IsProduction())
+        {
+            microsoftOptions.ClientId = Environment.GetEnvironmentVariable("MICROSOFT_CLIENT_ID")!;
+            microsoftOptions.ClientSecret = Environment.GetEnvironmentVariable("MICROSOFT_CLIENT_SECRET")!;
+        }
+        else
+        {
+            microsoftOptions.ClientId = configuration["MICROSOFT_CLIENT_ID"]!;
+            microsoftOptions.ClientSecret = configuration["MICROSOFT_CLIENT_SECRET"]!;
+        }
+    });
 
 var app = builder.Build();
 
