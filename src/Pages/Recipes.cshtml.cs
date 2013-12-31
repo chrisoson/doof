a498c786bc45948a54273d3d4b437e81dd1515b4
Chris <chris.oson.dev@gmail.com>
2014-01-01 00:00:00 +0500

    Got started on setting up localizor in different views

---
+++ src/Pages/Recipes.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Localization;
+
+namespace doof.Pages;
+
+public class Recipes : PageModel
+{
+
+    public void OnGet()
+    {
+    }
+}
